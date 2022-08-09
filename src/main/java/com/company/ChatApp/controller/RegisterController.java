3641476e0a9b5897c2e201f5a6eff2754d5f189f
abbaszade <muradabbaszade6@gmail.com>
2022-08-09 14:27:34 +0500

    login and register page added

--- src/main/java/com/company/ChatApp/controller/RegisterController.java
+++ src/main/java/com/company/ChatApp/controller/RegisterController.java
@@ -11,14 +11,36 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
-    
+
     @RequestMapping(method = RequestMethod.GET)
-    public String index(HttpServletRequest request,HttpServletResponse response){
-        return "register";
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("register");
+        return mv;
     }
-    
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public ModelAndView index1(@RequestParam(value = "name", required = false) String name,
+            @RequestParam(value = "email", required = false) String email, @RequestParam(value = "password", required = false) String password,
+            @RequestParam(value = "repassword", required = false) String repassword) {
+        ModelAndView mv = null;
+        RedirectView view=null;
+        if (password.equals(repassword)) {
+            mv = new ModelAndView("login");
+            view = new RedirectView("/login", true);
+        } else {
+            mv = new ModelAndView("register");
+            view = new RedirectView("/register", true);
+        }
+        view.setExposeModelAttributes(false);
+        mv.setView(view);
+        return mv;
+    }
+
 }
